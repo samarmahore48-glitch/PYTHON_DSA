67fb7d34521b3f0e0796eb394599013a56e43a33
Your Name <your_email@example.com>
2026-07-13 13:34:22 +0530

    initial

---
+++ DEGREE_CONVERSION.py
@@ -0,0 +1,6 @@
+celsius=int(input("enter the celsius value :- "))
+print(f"the value of fehrenheit is  {(1.8*celsius)+32} 'C")
+
+
+fehrenheit1=int(input("enter the fehrenheit value :- "))
+print(f"the value of celsius is  {0.556*(fehrenheit1-32)} 'F") 
